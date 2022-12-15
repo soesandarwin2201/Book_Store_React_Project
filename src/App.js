926f68099b18a0dafbe4d81a89b9703beabf1b53
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-12-15 19:03:08 +0630

    Make Navbar work with Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
+import Categories from './components/Categories';
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <Books />
+      <Routes>
+        <Route path='/categories' element={<Categories />}></Route>
+        <Route path='/' element={<Books />}></Route>
+      </Routes>
     </>
   );
 }
